f4d9f6a34e27d4e8f4643e6069df9aa1270ecab6
384113120@qq.com <lezhao@LedeMacBook-Pro.local>
2020-10-23 18:32:05 +0800

    更新相关内容

--- data_structure/实验1/a.cpp
+++ data_structure/实验1/a.cpp
@@ -9,46 +9,33 @@
 #include <math.h>
 #include <unordered_set>
 using namespace std;
-//保存所有的多项式：map保存数据，string提前存进数组里，以后每次query会比较快
-vector<map<float, float>> multi_;
+ vector<map<float, float>> multi_;
 vector<string> str_multi_;
-//创建一个多项式
-void amulti(map<float, float>& oneofmulti) {
+ void amulti(map<float, float>& oneofmulti) {
     string p;
     string e;
     cout << "请输入系数和幂";
-    //当输入#的时候跳出
-    //key为幂数，value为系数，采用map因为map自动排序，不采用ordered_map，因为遍历哈希表在显示多项式中不美观（次序没有排序），复杂度为O（logN）
-    //当然哈希表的效率会高很多，这里采用map，如果想加快速度，可以采用哈希表，这样在合并多项式的时候，查找的时间复杂度为O(1)，
-    while (1) {
-        //atoi(input.c_str())
-        cin >> p;
+                   while (1) {
+                 cin >> p;
         if (p == "#" || e == "#") return;
         cin >> e;
         if (p == "#" || e == "#") return;
-        //将p和e转换成int，这里没有考虑数据大小导致超过int的范围，可以使用long long扩大精度
-        float p_ = atof(p.c_str());
+                 float p_ = atof(p.c_str());
         float e_ = atof(e.c_str());
-        //if(oneofmulti.count(e_))
-        //幂数相同，系数相加
-        oneofmulti[e_] += p_;
+                          oneofmulti[e_] += p_;
     }
 }
 
-//显示多项式
-string showmulti(map<float, float> oneofmulti) {
+ string showmulti(map<float, float> oneofmulti) {
     string res = "";
     for (auto one : oneofmulti)
     {
-        //系数为0直接跳过
-        if (one.second == 0) continue;
-        //运用输出流将int类型转换成string
-        stringstream p;
+                 if (one.second == 0) continue;
+                 stringstream p;
         stringstream e;
         p << one.second;
         e << one.first;
-        //如果res为空串或者系数小于0，直接将string相加
-
+         
         if (res == "" || one.second < 0)
         {
             if (one.first != 0) {
@@ -58,8 +45,7 @@ string showmulti(map<float, float> oneofmulti) {
                 res += p.str();
             }
         }
-        //系数为正的话，需要加上加号
-        else
+                 else
         {
             if (one.first != 0) {
                 res += "+" + p.str() + "x^" + e.str();
@@ -68,16 +54,12 @@ string showmulti(map<float, float> oneofmulti) {
                 res += "+" + p.str();
             }
         }
-        //res+=itoa(one.second)+"x^"+itoa(one.first);
-    }
-    //返回结果
-    return res;
+             }
+         return res;
 }
 
-//多项式求和
-map<float, float> add2multi(map<float, float> thefirst, map<float, float> thesecond) {
-    //分别遍历thefirst和thesecond，并且将其信息记录在ans_multiply
-    map<float, float> ans_multiply;
+ map<float, float> add2multi(map<float, float> thefirst, map<float, float> thesecond) {
+         map<float, float> ans_multiply;
     for (auto one : thefirst)
     {
         const float e1 = one.first;
@@ -93,39 +75,30 @@ map<float, float> add2multi(map<float, float> thefirst, map<float, float> thesec
     return ans_multiply;
 }
 
-//微分
-map<float, float> differential(map<float, float> oneofmulti) {
+ map<float, float> differential(map<float, float> oneofmulti) {
     map<float, float> differential_it;
     for (auto one : oneofmulti)
     {
         float e = one.first;
         float p = one.second;
-        //由于在创建多项式的时候已经考虑了合并多项式的问题，这里仅仅考虑指数为1和0的情况
-        if (e == 0) continue;
+                 if (e == 0) continue;
         differential_it[e - 1] = p * e;
-        //这里采用在新的空间上保存微分的结果，如果原有结构上进行微分，最好按照指数的从小到大，以免发生数据的覆盖
-    }
+             }
     return differential_it;
 }
 
-//不定积分
-map<float, float> integral(map<float, float> oneofmulti) {
+ map<float, float> integral(map<float, float> oneofmulti) {
     map<float, float> integral_it;
     for (auto one : oneofmulti)
     {
         float e = one.first;
         float p = one.second;
-        integral_it[e + 1] += p / (e + 1);//！！！！！这要考虑p/（e+1）为小数，所以采用float类型的map更加好
-    }
-    //这里采用在新的空间上保存积分的结果，如果原有结构上进行积分，最好按照指数的从大到小，以免发生数据的覆盖
-    //return showmulti(integral_it)+"+contant";
-    return integral_it;
+        integral_it[e + 1] += p / (e + 1);     }
+              return integral_it;
 }
 
-//定积分
-float definite_integral(map<float, float> oneofmulti, float low, float high) {
-    //直接调用定积分的函数，并且将系数p和指数e分别用low和high求解
-    float ans_high = 0;
+ float definite_integral(map<float, float> oneofmulti, float low, float high) {
+         float ans_high = 0;
     float ans_low = 0;
     float sum = 0;
     map<float, float> oneofmulti_;
@@ -134,18 +107,12 @@ float definite_integral(map<float, float> oneofmulti, float low, float high) {
         float e = one.first;
         float p = one.second;
         sum += p * (pow(high, e) - pow(low, e));
-        //ans_high += p * pow(high, e);
-        //cout << p*pow(high,e) << endl;
-        //ans_low += p * pow(low, e);
-    }
+                               }
     return sum;
-    //return ans_high - ans_low;
-}
+     }
 
-//乘法
-map<float, float> multiply(map<float, float> thefirst, map<float, float> thesecond) {
-    //用thefirst中的每一个元素遍历thesecond的中每一个元素，系数相乘，指数相加。
-    map<float, float> ans_multiply;
+ map<float, float> multiply(map<float, float> thefirst, map<float, float> thesecond) {
+         map<float, float> ans_multiply;
     for (auto one : thefirst)
     {
         const float e1 = one.first;
@@ -160,10 +127,7 @@ map<float, float> multiply(map<float, float> thefirst, map<float, float> theseco
     return ans_multiply;
 }
 
-//乘方
-//乘方可以用牛顿二项式来简化整个运算过程，具体过程为：先将组合数用杨辉三角（动态规划）做出来，然后每次调用的时候减少时间复杂度；
-//下面的方法调用multiply简单写出该函数，但是时间复杂度较高，达到了n*n*m,哎.....；
-map<float, float> Nth_power(int m, map<float, float>oneofmulti) {
+   map<float, float> Nth_power(int m, map<float, float>oneofmulti) {
     map<float, float> temp = oneofmulti;
     while (--m) {
         temp = multiply(temp, oneofmulti);
@@ -171,10 +135,8 @@ map<float, float> Nth_power(int m, map<float, float>oneofmulti) {
     return temp;
 }
 
-//显示当前所有的多项式
-void show_all_multi() {
-    //直接遍历全局变量str_multi_，并且输出
-    cout << "当前存储的所有多项式为：" << endl;
+ void show_all_multi() {
+         cout << "当前存储的所有多项式为：" << endl;
     int i = 0;
     for (auto s : str_multi_)
     {
@@ -183,30 +145,23 @@ void show_all_multi() {
     }
 }
 
-//保存
-void savethemulti(map<float, float> oneofmulti) {
-    //将新的多项式直接加载str_multi_和multi_中
-    multi_.push_back(oneofmulti);
+ void savethemulti(map<float, float> oneofmulti) {
+         multi_.push_back(oneofmulti);
     str_multi_.push_back(showmulti(oneofmulti));
     return;
 }
 
-//删除
-void deletemulti() {
+ void deletemulti() {
     int index = 1;
-    //考虑性能，使用哈希set
-    unordered_set <int> deletelist;
-    //记录所有被删除多项式的index
-    while (index >= 0) {
+         unordered_set <int> deletelist;
+         while (index >= 0) {
         cin >> index;
-        //如果删除的index超过数组长度，则不做处理。
-        if (index >= (int)multi_.size())
+                 if (index >= (int)multi_.size())
             continue;
         else
             deletelist.insert(index);
     }
-    //创建新的vector保存未删除的多项式
-    vector<map<float, float>> tmp;
+         vector<map<float, float>> tmp;
     vector<string> tmp1;
     for (int i = 0; i < (int)multi_.size(); ++i)
     {
@@ -225,8 +180,7 @@ void deletemulti() {
     return;
 }
 
-//展示主界面
-void showthemeau() {
+ void showthemeau() {
     system("clear");
     cout << "功能：" << endl;
     cout << "a：创建新的多项式：" << endl;
@@ -244,8 +198,7 @@ void showthemeau() {
     cout << "z：退出计算器" << endl;
 }
 
-//创建多项式的界面
-void show_amulti() {
+ void show_amulti() {
     system("clear");
     cout << "请输入输入多项式的个数（返回主界面按0以后敲回车）：" << endl;
     int num = 0;
@@ -256,8 +209,7 @@ void show_amulti() {
     cout << "请输入多项式（以'#'结束）：" << endl;
     cout << "示例：3x^4 - 0.5x^0.6 + 7" << endl;
     cout << "输入：3 4 -0.5 0.6 7 0 #" << endl;
-    //调用创建多项式的函数
-    for (int i = 0; i < num; ++i) {
+         for (int i = 0; i < num; ++i) {
         map<float, float> tmp;
         amulti(tmp);
         savethemulti(tmp);
@@ -265,8 +217,7 @@ void show_amulti() {
     return showthemeau();
 }
 
-//删除多项式的界面
-void show_delete_multi() {
+ void show_delete_multi() {
     system("clear");
     show_all_multi();
     cout << "请输入你要删除的多项式的序号(以-1结束，示例：0 -1敲回车)：" << endl;
@@ -279,8 +230,7 @@ void show_delete_multi() {
     return showthemeau();
 }
 
-//显示特定的多项式界面
-void show_n_multi() {
+ void show_n_multi() {
     system("clear");
     cout << "请输入显示多项式的index：" << endl;
     int len = str_multi_.size() - 1;
@@ -294,8 +244,7 @@ void show_n_multi() {
     return showthemeau();
 }
 
-//显示微分界面
-void show_diff() {
+ void show_diff() {
     system("clear");
     int len = str_multi_.size() - 1;
     cout << "请输入你要进行微分的多项式的index(0~" << len << "):" << endl;
@@ -310,12 +259,10 @@ void show_diff() {
     cin >> keyborad_in;
     if (keyborad_in == 1)
         savethemulti(diffone);
-    //else
-    return showthemeau();
+         return showthemeau();
 }
 
-//显示不定积分界面
-void show_inte() {
+ void show_inte() {
     system("clear");
     int len = str_multi_.size() - 1;
     cout << "请输入你要进行不定积分的多项式的index(0~" << len << "):" << endl;
@@ -330,12 +277,10 @@ void show_inte() {
     cin >> keyborad_in;
     if (keyborad_in == 1)
         savethemulti(inteone);
-    //else
-    return showthemeau();
+         return showthemeau();
 }
 
-//显示定积分的界面
-void show_definite_integral() {
+ void show_definite_integral() {
     system("clear");
     int len = str_multi_.size() - 1;
     cout << "请输入你要进行定积分的多项式的index(0~" << len << "):" << endl;
@@ -354,8 +299,7 @@ void show_definite_integral() {
     return showthemeau();
 }
 
-//显示乘方界面
-void show_Nth_power() {
+ void show_Nth_power() {
     system("clear");
     int len = str_multi_.size() - 1;
     cout << "请输入你要进行乘方的多项式的index(0~" << len << "):" << endl;
@@ -380,12 +324,10 @@ void show_Nth_power() {
     cin >> keyborad_in;
     if (keyborad_in == 1)
         savethemulti(powone);
-    //else
-    return showthemeau();
+         return showthemeau();
 }
 
-//显示乘法界面
-void show_multiply() {
+ void show_multiply() {
     system("clear");
     vector<int> index;
     int len = str_multi_.size() - 1;
@@ -411,12 +353,10 @@ void show_multiply() {
     cin >> keyborad_in;
     if (keyborad_in == 1)
         savethemulti(res);
-    //else
-    return showthemeau();
+         return showthemeau();
 }
 
-//显示求和界面add2multi
-void show_add2multi() {
+ void show_add2multi() {
     system("clear");
     vector<int> index;
     int len = str_multi_.size() - 1;
@@ -442,21 +382,17 @@ void show_add2multi() {
     cin >> keyborad_in;
     if (keyborad_in == 1)
         savethemulti(res);
-    //else
-    return showthemeau();
+         return showthemeau();
 
 }
 
-//控制菜单
-void switchit(char keyboard_in) {
+ void switchit(char keyboard_in) {
     switch (keyboard_in) {
-    case 'a'://a：创建新的多项式
-        show_amulti();
+    case 'a':         show_amulti();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'b'://b：显示当前所有的多项式
-        system("clear");
+    case 'b':         system("clear");
         show_all_multi();
         cout << "输入任意键敲回车返回主界面" << endl;
         cin >> keyboard_in;
@@ -464,48 +400,39 @@ void switchit(char keyboard_in) {
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'c'://c：删除多项式
-        show_delete_multi();
+    case 'c':         show_delete_multi();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'd'://d：显示第n个多项式
-        show_n_multi();
+    case 'd':         show_n_multi();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'e'://e：微分
-        show_diff();
+    case 'e':         show_diff();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'f'://f：定积分
-        show_definite_integral();
+    case 'f':         show_definite_integral();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'g'://g：不定积分
-        show_inte();
+    case 'g':         show_inte();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'h'://h：乘方
-        show_Nth_power();
+    case 'h':         show_Nth_power();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'i'://i：求和
-        show_add2multi();
+    case 'i':         show_add2multi();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'j'://j：乘法
-        show_multiply();
+    case 'j':         show_multiply();
         cin >> keyboard_in;
         switchit(keyboard_in);
         break;
-    case 'z'://z：退出计算器
-        cout << "谢谢使用" << endl;
+    case 'z':         cout << "谢谢使用" << endl;
         system("clear");
         return;
         break;
@@ -520,37 +447,11 @@ void switchit(char keyboard_in) {
     }
 }
 int main() {
-    //调试
-    multi_.clear();
+         multi_.clear();
     showthemeau();
     cout << "输入功能字母" << endl;
     char keyboard_in;
     cin >> keyboard_in;
     switchit(keyboard_in);
 
-    //show_amulti();
-    //show_delete_multi();
-    //show_n_multi();
-    //show_diff();
-    //show_inte();
-    //show_Nth_power();
-    //show_add2multi();
-    //show_all_multi();
-    //definite_integral();
-    /*
-    vector<map<float,float>> multi_;
-    map<float,float>tmp;
-    amulti(tmp);
-    map<float,float>tmp2;
-    amulti(tmp2);
-    multi_.push_back(tmp);
-    multi_.push_back(tmp2);
-    cout << "第一个多项式为"<<showmulti(multi_[0])<<endl;
-    cout << "第二个多项式为"<<showmulti(multi_[1])<<endl;
-    */
-    //cout << "两个多项式的乘法为"<<showmulti(multiply(multi_[0],multi_[1]))<<endl;
-    //cout << "乘方"<< showmulti(Nth_power(2,multi_[0]))<<endl;
-    //cout << "微分"<< showmulti(differential(multi_[0]))<<endl;
-    //cout << "不定积分"<< showmulti(integral(multi_[0]))+"+contant"<<endl;
-    //cout << "定积分"<<definite_integral(multi_[0],1,2)<<endl;
-}
+        }
